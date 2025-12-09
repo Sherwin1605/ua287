6984f1773bcfe565b38b817ce10c2d8f4104064d
Sherwin1605 <96462480+Sherwin1605@users.noreply.github.com>
2025-12-09 06:24:12 +0000

    `

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-java -Xmx12G -jar fabric-server-mc.1.21.10-loader.0.18.1-launcher.1.1.0.jar nogui
+java -Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar fabric-server-mc.1.21.1-loader.0.18.1-launcher.1.1.0.jar nogui
+pause
